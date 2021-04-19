7a86438f0815f75c2b33854a974190985389c2cd
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2021-04-19 15:13:05 +0900

    [IMP] base_tier_validation (#7)

--- base_tier_validation/__manifest__.py
+++ base_tier_validation/__manifest__.py
@@ -3,23 +3,29 @@
 {
     "name": "Base Tier Validation",
     "summary": "Implement a validation process based on tiers.",
-    "version": "12.0.3.5.3",
+    "version": "12.0.3.7.0",
     "development_status": "Mature",
-    "maintainers": ["LoisRForgeFlow"],
+    "maintainers": ['LoisRForgeFlow'],
     "category": "Tools",
     "website": "https://github.com/OCA/server-ux",
     "author": "Eficent, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": ["mail"],
+    "depends": [
+        "mail"
+    ],
     "data": [
         "data/mail_data.xml",
+        "security/tier_validation_security.xml",
         "security/ir.model.access.csv",
         "views/tier_definition_view.xml",
         "views/tier_review_view.xml",
         "views/assets_backend.xml",
         "wizard/comment_wizard_view.xml",
     ],
-    "qweb": ["static/src/xml/systray.xml", "static/src/xml/tier_review_template.xml",],
+    'qweb': [
+        'static/src/xml/systray.xml',
+        'static/src/xml/tier_review_template.xml',
+    ],
 }
