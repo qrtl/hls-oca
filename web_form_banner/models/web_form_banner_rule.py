c853e4fd68d670fd69e14c64ce29269d85e77b7c
yostashiro <tashiro@quartile.co>
2025-09-16 13:37:18 +0000

    reallocate some vars from _get_banner_eval_context() to _banner_base_eval_ctx_static()

--- web_form_banner/models/web_form_banner_rule.py
+++ web_form_banner/models/web_form_banner_rule.py
@@ -100,6 +100,9 @@ class WebFormBannerRule(models.Model):
                 "relativedelta": relativedelta,
             },
             "timezone": timezone,
+            "float_compare": float_compare,
+            "float_is_zero": float_is_zero,
+            "float_round": float_round,
         }
 
     @api.model
@@ -115,9 +118,6 @@ class WebFormBannerRule(models.Model):
                 "context_today": lambda ts=None: fields.Date.context_today(
                     record, timestamp=ts
                 ),
-                "float_compare": float_compare,
-                "float_is_zero": float_is_zero,
-                "float_round": float_round,
                 "url_for": self._build_form_url,
             }
         )
