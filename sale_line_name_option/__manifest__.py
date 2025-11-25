0f32e3e56a53778d22b0cdaabbff1ed8cd402e7a
Aungkokolin1997 <lin@quartile.co>
2025-11-25 02:27:42 +0000

    [ADD] sale_line_name_option

---
+++ sale_line_name_option/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2025 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Line Name Option",
+    "author": "Quartile, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "category": "Sales",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": ["views/res_config_settings_views.xml"],
+    "maintainers": ["yostashiro", "aungkokolin1997"],
+    "installable": True,
+}
