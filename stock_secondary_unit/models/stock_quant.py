eee0f46c0aeacfdec1492c05476b5c78cce93ff1
yostashiro <tashiro@quartile.co>
2025-12-13 09:15:45 +0000

    [IMP] stock_secondary_unit: fixup!

--- stock_secondary_unit/models/stock_quant.py
+++ stock_secondary_unit/models/stock_quant.py
@@ -2,29 +2,19 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
-from odoo.tools.float_utils import float_round
 
 
 class StockQuant(models.Model):
-    _inherit = "stock.quant"
+    _inherit = ["stock.quant", "product.secondary.unit.mixin"]
+    _name = "stock.quant"
+    _secondary_unit_fields = {"qty_field": "quantity", "uom_field": "product_uom_id"}
 
-    secondary_uom_qty = fields.Float(
-        string="Secondary Qty",
-        digits="Product Unit of Measure",
-        compute="_compute_secondary_uom_qty",
-        store=True,
-    )
     secondary_uom_id = fields.Many2one(
-        related="product_id.stock_secondary_uom_id", string="Secondary UoM"
+        related="product_id.stock_secondary_uom_id",
     )
 
-    @api.depends("quantity", "secondary_uom_id")
-    def _compute_secondary_uom_qty(self):
-        for quant in self.filtered(lambda q: q.secondary_uom_id):
-            uom = quant.product_uom_id
-            secondary_uom = quant.secondary_uom_id
-            factor = secondary_uom.factor * uom.factor
-            quant.secondary_uom_qty = float_round(
-                quant.quantity / (factor or 1.0),
-                precision_rounding=secondary_uom.uom_id.rounding,
-            )
+    @api.model
+    def _get_secondary_uom_qty_depends(self):
+        return super()._get_secondary_uom_qty_depends() + [
+            "product_id.stock_secondary_uom_id"
+        ]
