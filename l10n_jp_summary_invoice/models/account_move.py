b5af35061b78ede41ac123cda1a0f1b88ae9cf25
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2026-03-14 10:17:13 +0900

    [18.0][5862][ADD] l10n_jp_summary_invoice (#89)
    [18.0][5862][ADD] l10n_jp_summary_invoice (#89)

---
+++ l10n_jp_summary_invoice/models/account_move.py
@@ -0,0 +1,33 @@
+# Copyright 2025 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    is_not_for_billing = fields.Boolean(
+        compute="_compute_is_not_for_billing",
+        store=True,
+        readonly=False,
+        help="If selected, the invoice is excluded from the billing process.",
+    )
+
+    @api.depends("partner_id")
+    def _compute_is_not_for_billing(self):
+        for move in self:
+            # Intentionally not using move.commercial_partner_id to allow more granular
+            # control
+            move.is_not_for_billing = move.partner_id.is_not_for_billing
+
+    def _get_partner_bank(self):
+        partner_banks = self.mapped("partner_bank_id")
+        if len(partner_banks) > 1:
+            raise UserError(_("Please select invoices with the same recipient bank."))
+        return partner_banks
+
+    def action_create_billing(self):
+        self._get_partner_bank()
+        return super().action_create_billing()
