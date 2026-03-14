e27027a3a866bfb23a73bf5ee57e2d68428fb39a
kanda999 <kanda@quartile.co>
2026-03-14 01:15:58 +0000

    fix pre-commit

--- sale_order_line_client_order_ref/models/sale_order_line.py
+++ sale_order_line_client_order_ref/models/sale_order_line.py
@@ -1,7 +1,7 @@
 # Copyright 2025 Quartile (https://www.quartile.co)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
+from odoo import api, fields, models
 
 
 class SaleOrderLine(models.Model):
