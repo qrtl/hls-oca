57c5a37d91e51f2b7aaa92630cba694964ceaf65
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2026-03-14 08:15:42 +0700

    [18.0][5936][ADD] sale_commercial_partner (#84)
    [18.0][5936][ADD] sale_commercial_partner (#84)

---
+++ sale_commercial_partner/models/sale_order.py
@@ -0,0 +1,33 @@
+# Copyright 2016-2022 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    commercial_partner_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Customer Entity",
+        related="partner_id.commercial_partner_id",
+        store=True,
+        index=True,
+    )
+    partner_invoice_domain = fields.Binary(compute="_compute_partner_domains")
+    partner_shipping_domain = fields.Binary(compute="_compute_partner_domains")
+
+    @api.depends("commercial_partner_id", "company_id")
+    def _compute_partner_domains(self):
+        for order in self:
+            order.partner_invoice_domain = []
+            order.partner_shipping_domain = []
+            if order.company_id.use_invoice_commercial_partner_filter:
+                order.partner_invoice_domain = [
+                    ("commercial_partner_id", "=", order.commercial_partner_id.id)
+                ]
+            if order.company_id.use_shipping_commercial_partner_filter:
+                order.partner_shipping_domain = [
+                    ("commercial_partner_id", "=", order.commercial_partner_id.id)
+                ]
