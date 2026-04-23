bb6fbc3a9da9f2b659a1f4d45e12203479f12014
Aungkokolin1997 <lin@quartile.co>
2026-04-23 02:40:02 +0000

    [IMP] l10n_jp_summary_invoice: remove billing_line_ids

--- l10n_jp_summary_invoice/models/account_move.py
+++ l10n_jp_summary_invoice/models/account_move.py
@@ -14,12 +14,6 @@ class AccountMove(models.Model):
         readonly=False,
         help="If selected, the invoice is excluded from the billing process.",
     )
-    # TODO: This field should be moved to account_billing module.
-    billing_line_ids = fields.One2many(
-        comodel_name="account.billing.line",
-        inverse_name="move_id",
-        string="Billing Lines",
-    )
     billing_id = fields.Many2one(
         comodel_name="account.billing",
         compute="_compute_billing_id",
