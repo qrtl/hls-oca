7f13d85718b7975386034ba92c4e3e63b36e9afe
Aungkokolin1997 <lin@quartile.co>
2026-05-21 07:28:09 +0000

    [6794][IMP] report_pdf_zip_download: reflect OCA changes

--- report_pdf_zip_download/static/src/js/action_manager_report.esm.js
+++ report_pdf_zip_download/static/src/js/action_manager_report.esm.js
@@ -1,6 +1,7 @@
 // © 2017 Creu Blanca
 // Copyright 2024 Quartile (https://www.quartile.co)
 // License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+/* global URLSearchParams */
 import {download} from "@web/core/network/download";
 import {registry} from "@web/core/registry";
 import {user} from "@web/core/user";
